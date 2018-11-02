4b9fef619e3c08616b929db827e7c94919d7caf6
mson97 <mson97@users.noreply.github.com>
2018-11-01 23:57:50 -0700

    Add Translator
    begin output of java program

---
+++ src/com/company/Translator.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Translator {
+    public void prog_start() {
+        System.out.println("public class My_e2j{");
+        System.out.println("public static void main(String[] args{" );
+    }
+    public void prog_end() {
+        System.out.println("}");
+        System.out.println("}");
+    }
+}
