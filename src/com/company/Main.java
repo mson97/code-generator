ba08351c33257857d2ddd35da53dc94fa5364952
mson97 <mson97@users.noreply.github.com>
2018-11-01 23:37:02 -0700

    Paste contents into files
    transfer previous part's code into starting files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String args[]) {
+        Scan scanner = new Scan();
+        new Parser(scanner);
     }
 }
